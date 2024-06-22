9b6007feb904d7a8e2279c0a5b6804bcdf864190
tcorzo <t.corzo.personal@gmail.com>
2024-06-22 11:43:20 -0300

    Added submission API methods

--- myrpl_cli/myrpl_api.py
+++ myrpl_cli/myrpl_api.py
@@ -1,8 +1,12 @@
 import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import mimetypes
 
 BASE_URL = 'https://myrpl.ar'
 
 class MYRPL_API:
+    headers = {}
+
     def __init__(self, bearer_token) -> None:
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0',
@@ -37,3 +41,46 @@ class MYRPL_API:
         file_content = response.json()
 
         return file_content
+
+    def fetch_submissions(self, course_id, activity_id):
+        submissions_url = f'{BASE_URL}/api/courses/{course_id}/activities/{activity_id}/submissions'
+        response = requests.get(submissions_url, headers=self.headers)
+        submissions = response.json()
+
+        return submissions
+
+    def fetch_final_submission(self, course_id, activity_id):
+        final_submission_url = f'{BASE_URL}/api/courses/{course_id}/activities/{activity_id}/finalSubmission'
+        response = requests.get(final_submission_url, headers=self.headers)
+        final_submission = response.json()
+
+        return final_submission
+
+    def fetch_submission_result(self, submission_id):
+        result_url = f'{BASE_URL}/api/submissions/{submission_id}/result'
+        response = requests.get(result_url, headers=self.headers)
+
+        return response.json()
+
+    def submit(self, course_id, activity_id, submission_file, description=""):
+        submit_url = f'{BASE_URL}/api/courses/{course_id}/activities/{activity_id}/submissions'
+
+        # Determine the MIME type of the file
+        mime_type, _ = mimetypes.guess_type(submission_file)
+        if mime_type is None:
+            mime_type = 'application/octet-stream'
+
+        with open(submission_file, 'rb') as f:
+            form = MultipartEncoder(
+                fields={
+                    'file': (submission_file, f, mime_type),
+                    'description': description
+                }
+            )
+
+        headers = self.headers.copy()
+        headers['Content-Type'] = form.content_type
+
+        response = requests.post(submit_url, headers=headers, data=form)
+
+        return response.json()
